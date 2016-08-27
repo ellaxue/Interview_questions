5ccefea0dd235760ac4a151befe6a29aa0b2e70e
Ella Xue <ellaxue007@gmail.com>
2016-08-27 12:21:32 -0400

    cleaned up code

--- add_nums_to_target.cpp
+++ add_nums_to_target.cpp
@@ -5,6 +5,9 @@
  *      Author: ellaxue
  */
 
+/* Find a pair of numbers in the array that add up to a target value
+ * return the vector of indexs of that pair (i, j)
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -19,31 +22,12 @@ public:
 	{
 		std::unordered_map<int, int> hashTable;
 		std::vector<int> result;
-
-//		for( auto it = numbers.begin(); it != numbers.end(); it++)
-//		{
-//			if(hashTable.count(target - *it))
-//			{
-//				result[0]= hashTable[target - *it];
-//				result[1] = (int)(it - numbers.begin());
-//				cout<<"it" <<(int)(numbers.begin())<<endl;
-//				return result;
-//			}
-//			hashTable[*it] = (int)(it - numbers.begin());
-//		}
-
 		for(int i = 0; i < numbers.size(); i++)
-		{printf("if current number  = %d \n", numbers[i]);
-
+		{
 			if(hashTable.count(target - numbers[i]))
 			{
-				printf("target   = %d \n", (target - numbers[i]));
 				result.push_back(hashTable[target - numbers[i]]);
-
 				result.push_back(i);
-								for (std::vector<int>::iterator it = result.begin(); it != result.end(); ++it)
-							    std::cout << ' ' << *it;
-							  std::cout << '\n';
 				return result;
 			}
 			hashTable[numbers[i]] = i;
@@ -58,10 +42,13 @@ int main(void)
 	std::vector<int> num1 (myints1, myints1 + sizeof(myints1) / sizeof(int));
 	solution *s = new solution();
 	num1 = s->addToTarget(num1, 16);
+
 	printf("Final solution: " );
-			for (std::vector<int>::iterator it = num1.begin(); it != num1.end(); ++it)
-			    std::cout << ' ' << *it;
-			  std::cout << '\n';
+	for (std::vector<int>::iterator it = num1.begin(); it != num1.end(); ++it)
+	{
+		std::cout << ' ' << *it;
+	}
+		std::cout << '\n';
 
 	return 0;
 }
